10e8ee9fc91763d18c54812e7982b7a3a97218ea
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-05-07 16:43:06 +0800

    2

--- Tsecret.lua
+++ Tsecret.lua
@@ -15,6 +15,9 @@ if RequiredScript == "core/lib/managers/mission/coremissionscriptelement" then
 			managers.mission._scripts["baldwin"]._elements[104136]:on_executed()
 		elseif Global.level_data.level_id == "diamond_heist" and self._id == 101467 then
 			self._chance, self._values.chance = nil
+		elseif Global.level_data.level_id == "suburbia" and self._id == 102102 then
+			self:get_mission_element(102101):on_executed()
+			return
 		end
 		OldFunc(self, instigator)
 	end
