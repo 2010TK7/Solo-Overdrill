55c1fa859bd1ed12c9c4dcb61717e94673ece15e
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-05-08 17:29:13 +0800

    4

--- Tsecret/base.lua
+++ Tsecret/base.lua
@@ -1,3 +1,3 @@
-local module = DorHUDMod:new("Tsecret", {name = "Solo Overdrill", author = "TK7", description = "https://modworkshop.net/mod/27472", version = "3"})
+local module = DorHUDMod:new("Solo Overdrill", {author = "TK7", description = "https://modworkshop.net/mod/27472", version = "4"})
 module:hook_post_require("core/lib/managers/mission/coremissionscriptelement", "coremissionscriptelement")
 return module
